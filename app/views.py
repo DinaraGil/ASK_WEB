f4d2d6e12f51458dc97e85e68716f63c129922d6
DinaraGil <dinaragig03@gmail.com>
2025-04-08 21:36:57 +0300

    Added paggination, questions, authorized user's header and new html pages.

--- app/views.py
+++ app/views.py
@@ -1,11 +1,24 @@
+from django.contrib.admin.templatetags.admin_list import pagination_tag
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
+questions = []
+for i in range(0,29):
+  questions.append({
+    'title': 'title ' + str(i),
+    'id': i,
+    'text': 'text' + str(i),
+    'img_path': "/img/concetemixer.jpg"
+  })
 
 def index(request):
-    return render(request, 'index.html')
+    page_num = int(request.GET.get('page', 1))
+    paginator = Paginator(questions, 5)
+    page = paginator.page(page_num)
+    return render(request, 'index.html', context={'questions': page.object_list, 'page_obj': page})
 
 
 def base(request):
@@ -14,3 +27,22 @@ def base(request):
 
 def ask(request):
     return render(request, 'ask.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def settings(request):
+    return render(request, 'settings.html')
+
+
+def hot(request):
+    page_num = int(request.GET.get('page', 1))
+    paginator = Paginator(questions[::-1], 5)
+    page = paginator.page(page_num)
+    return render(request, 'hot.html', context={'questions': page.object_list, 'page_obj': page})
+
+def question(request, question_id):
+    return render(request, 'single_question.html', context={'question': questions[question_id]})
--- ask_web/urls.py
+++ ask_web/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('base', views.base, name='base'),
     path('ask', views.ask, name='ask'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('hot', views.hot, name='hot'),
+    path('question/<int:question_id>', views.question, name='question'),
+    path('settings', views.settings, name='settings'),
 ]
